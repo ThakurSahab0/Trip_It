353a0927a102ac84a3df563e73e3dae74757e830
Varun Jadon <varunjadon.se@gmail.com>
2023-02-18 14:23:29 +0530

    NEW THINGS ADDED

--- lib/misc/pages/nav_pages/search_page.dart
+++ lib/misc/pages/nav_pages/search_page.dart
@@ -12,16 +12,33 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setBackgroundColor(const Color(0x00000000))
+  ..setNavigationDelegate(
+  NavigationDelegate(
+  onProgress: (int progress) {
+  // Update loading bar.
+  },
+  onPageStarted: (String url) {},
+  onPageFinished: (String url) {},
+  onWebResourceError: (WebResourceError error) {},
+  onNavigationRequest: (NavigationRequest request) {
+  if (request.url.startsWith('https://www.google.com/')) {
+  return NavigationDecision.prevent;
+  }
+  return NavigationDecision.navigate;
+  },
+  ),
+  )
+  ..loadRequest(Uri.parse('https://www.google.com/'));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Search"),
       ),
-      body: WebView(
-        javascriptMode: JavascriptMode.unrestricted,
-        initialUrl: "https://www.google.com/",
-      ),
+      body: WebViewWidget(controller: controller),
     );
   }
 }
